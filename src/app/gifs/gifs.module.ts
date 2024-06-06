8578fa5436f067a301674af8ebd5ba3bd3c50987
visuaLangley <millarpjorge@gmail.com>
2024-06-06 01:33:55 -0400

    Dependencia de componente card con card-list

--- src/app/gifs/gifs.module.ts
+++ src/app/gifs/gifs.module.ts
@@ -4,7 +4,7 @@ import { CommonModule } from '@angular/common';
 import { HomePageComponent } from './pages/home/home-page.component';
 import { SearchBoxComponent } from './components/search-box/search-box.component';
 import { CardListComponent } from './components/card-list/card-list.component';
-import { GifsService } from './services/gifs.service';
+import { CardComponent } from './components/card/card.component';
 
 
 
@@ -14,6 +14,7 @@ import { GifsService } from './services/gifs.service';
     HomePageComponent,
     SearchBoxComponent,
     CardListComponent,
+    CardComponent,
 
 
   ],
